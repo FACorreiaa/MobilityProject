bebb878ea9c53ccc02552c0df05205627874caf9
Knox316 <fernandocorreia316@gmail.com>
2019-12-15 15:30:04 +0000

    added configuration to mlab

--- MobilityProject/src/Server.ts
+++ MobilityProject/src/Server.ts
@@ -4,14 +4,26 @@ import { Request, Response } from 'express';
 import logger from 'morgan';
 import path from 'path';
 import BaseRouter from './routes';
+import mongoose from 'mongoose';
 
 // Init express
 const app = express();
 
+//BD SETUP
+const MONGO_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-krbnl.mongodb.net/test?retryWrites=true&w=majority`;
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+mongoose.connection
+  .once('open', () => console.log('Connected to MongoLab instance.'))
+  .on('error', error => console.log('Error connecting to MongoLab:', error));
+
 // Add middleware/settings/routes to express.
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/api', BaseRouter);
 
@@ -27,7 +39,7 @@ app.set('views', viewsDir);
 const staticDir = path.join(__dirname, 'public');
 app.use(express.static(staticDir));
 app.get('*', (req: Request, res: Response) => {
-    res.sendFile('index.html', {root: viewsDir});
+  res.sendFile('index.html', { root: viewsDir });
 });
 
 // Export express instance
