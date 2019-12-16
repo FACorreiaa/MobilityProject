21d3d523ff1959305534326b61669efb91b536c1
Knox316 <fernandocorreia316@gmail.com>
2019-12-16 14:29:49 +0000

    BUILD -

--- dist/index.js
+++ dist/index.js
@@ -4,9 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 require("./LoadEnv");
-const _server_1 = __importDefault(require("@server"));
+const server_1 = __importDefault(require("src/server"));
 const _shared_1 = require("@shared");
 const port = Number(process.env.PORT || 5000);
-_server_1.default.listen(port, () => {
+server_1.default.listen(port, () => {
     _shared_1.logger.info('Express server started on port: ' + port);
 });
