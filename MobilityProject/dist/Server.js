6c02419fe4a4c6222f4e3f85562984b30d1e1274
Knox316 <fernandocorreia316@gmail.com>
2019-12-16 12:17:34 +0000

    trying dist

---
+++ MobilityProject/dist/Server.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tslib_1 = require("tslib");
+const cookie_parser_1 = tslib_1.__importDefault(require("cookie-parser"));
+const express_1 = tslib_1.__importDefault(require("express"));
+const morgan_1 = tslib_1.__importDefault(require("morgan"));
+const path_1 = tslib_1.__importDefault(require("path"));
+const routes_1 = tslib_1.__importDefault(require("./routes"));
+const mongoose_1 = tslib_1.__importDefault(require("mongoose"));
+const app = express_1.default();
+const MONGO_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-krbnl.mongodb.net/test?retryWrites=true&w=majority`;
+mongoose_1.default.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+mongoose_1.default.connection
+    .once('open', () => console.log('Connected to MongoLab instance.'))
+    .on('error', error => console.log('Error connecting to MongoLab:', error));
+app.use(morgan_1.default('dev'));
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use(cookie_parser_1.default());
+app.use('/api', routes_1.default);
+const viewsDir = path_1.default.join(__dirname, 'views');
+app.set('views', viewsDir);
+const staticDir = path_1.default.join(__dirname, 'public');
+app.use(express_1.default.static(staticDir));
+app.get('*', (req, res) => {
+    res.sendFile('index.html', { root: viewsDir });
+});
+exports.default = app;
