41193952cc7c5d78a23d48f13ba992f77a8522e7
Knox316 <fernandocorreia316@gmail.com>
2020-01-25 21:32:31 +0000

    fix: 🐛 pusher
    pusher

--- client/src/components/Dashboard/Charts.js
+++ client/src/components/Dashboard/Charts.js
@@ -2,102 +2,89 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { logoutUser } from '../../actions/authActions';
-import { getOccupancy,getCheckinDash } from '../../actions/dashActions';
+import { getOccupancy, getCheckinDash } from '../../actions/dashActions';
 import Pusher from 'pusher-js';
 import { Bar } from 'react-chartjs-2';
-
 
 Pusher.logToConsole = true;
 
 let channel;
 
-
 class Charts extends Component {
   constructor(props) {
-    super(props);  
-    this.state= {options: []};
+    super(props);
+    this.state = { options: [] };
   }
 
-    componentDidMount(){
+  componentDidMount() {
     this.props.getOccupancy();
-    const { charts_places } = this.props.charts_places;
-    
-
     var pusher = new Pusher('da84b590e82a4f23838b', {
       cluster: 'eu',
       forceTLS: true
     });
-     channel = pusher.subscribe('occupancy');
-     channel.bind('update-places', res => {
+    channel = pusher.subscribe('occupancy');
+    channel.bind('update-places', res => {
+      console.log('RES', res);
+      this.setState({ options: res.values });
 
-      //alert(JSON.stringify(res));
-     let array = [10,10];
-      console.log('no pusher res = ' + array);
-     
-      this.setState({ options: array });
-
+      console.log('statSTATEEEEEEEEEEEEEEEEE' + JSON.stringify(this.state));
     });
   }
- 
 
   render() {
     const { charts_places } = this.props.charts_places;
-    console.log('dentro do render charts_places.data: '+ charts_places.data)
-    this.state = { options: charts_places.data};
-    //this.state = {options: charts_places.data };
-    console.log('this.state.options no render = ' + this.state.options );
-
+    console.log(this.state);
     const { user } = this.props.auth;
     return (
       <div style={{ height: '75vh' }}>
         <div className='row'>
-          <div >
+          <div>
             <h4>
-              <b style={{ display: 'flex' }} > Hey there, {user.username}</b> 
-              <p style={{ display: 'flex' }} className='flow-text grey-text text-darken-1'>
+              <b style={{ display: 'flex' }}> Hey there, {user.username}</b>
+              <p
+                style={{ display: 'flex' }}
+                className='flow-text grey-text text-darken-1'
+              >
                 Mobility Charts Dashboard
               </p>
             </h4>
             <br></br>
-            <div>
-          </div>
+            <div></div>
           </div>
         </div>
-        <div class="container">
-        </div>
+        <div class='container'></div>
         <br></br>
         <div>
-        <Bar
-          data= {{
-            labels: ['January', 'February'],
-            datasets: [
-              {
-                label: 'Rainfall',
-                backgroundColor: 'rgba(75,192,192,1)',
-                borderColor: 'rgba(0,0,0,1)',
-                borderWidth: 2,
-                data: this.state.options
+          <Bar
+            data={{
+              labels: ['January', 'February'],
+              datasets: [
+                {
+                  label: 'Rainfall',
+                  backgroundColor: 'rgba(75,192,192,1)',
+                  borderColor: 'rgba(0,0,0,1)',
+                  borderWidth: 2,
+                  data: this.state.options
+                }
+              ]
+            }}
+            options={{
+              title: {
+                display: true,
+                text: 'Average Rainfall per month',
+                fontSize: 20
+              },
+              legend: {
+                display: true,
+                position: 'right'
               }
-            ]
-          }}
-          options={{
-            title:{
-              display:true,
-              text:'Average Rainfall per month',
-              fontSize:20
-            },
-            legend:{
-              display:true,
-              position:'right'
-            }
-          }}
-        />
+            }}
+          />
         </div>
-     </div>
+      </div>
     );
   }
 }
-
 
 Charts.propTypes = {
   logoutUser: PropTypes.func.isRequired,
@@ -111,4 +98,8 @@ const mapStateToProps = state => ({
   charts_places: state.charts_places
 });
 //connect to redux
-export default connect(mapStateToProps, { logoutUser,getOccupancy, getCheckinDash })(Charts);
+export default connect(mapStateToProps, {
+  logoutUser,
+  getOccupancy,
+  getCheckinDash
+})(Charts);
