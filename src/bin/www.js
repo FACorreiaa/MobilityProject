faba20193756475f07ac44bb14793e511d92bdfe
Knox316 <fernandocorreia316@gmail.com>
2020-02-01 20:20:12 +0000

    change folder struct

--- src/bin/www.js
+++
@@ -1,9 +0,0 @@
-// bin/www.js
-/**
- * Module dependencies.
- */
-import app from '../app';
-import debugLib from 'debug';
-import http from 'http';
-const debug = debugLib('your-project-name:server');
-// generated code below.
