8ce10f9b219b4f208ce9de6bc1b22941373d8c8d
Knox316 <fernandocorreia316@gmail.com>
2020-02-02 09:03:52 +0000

    fix: 🐛 remove client folder

--- client/src/components/Contants/constants/typo.js
+++
@@ -1,8 +0,0 @@
-import { makeStyles } from "@material-ui/core/styles";
-
-export const typo = makeStyles(theme => ({
-  root: {
-    width: "100%",
-    maxWidth: 500
-  }
-}));
--- client/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import 'materialize-css/dist/js/materialize.min.js';
-import './styles/tailwind.css';
-
-ReactDOM.render(<App />, document.querySelector('#root'));
